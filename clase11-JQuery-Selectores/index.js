cf9f08101ba6ab02f355b4c4a45581e707a79750
Walter Silva <edweb.mza@gmail.com>
2024-11-06 07:37:11 -0300

    Seleccionamos una <ul> con clase y que tenga un hijo <li>

--- clase11-JQuery-Selectores/index.js
+++ clase11-JQuery-Selectores/index.js
@@ -10,3 +10,16 @@ $('.ulFooter')
 $('li')
 
 
+//                                  Combinacion de Selectores
+
+//                  Selecciona todos los p
+$('p')
+
+//                  Selecciona todos los a
+$('a')
+
+//                  Selecciona todos los <p> y los <a>
+$('p , a')
+
+//                  Seleccioan un ul con clase ulFooter, que tenga un hijo <li>
+$('ul.ulFooter li')
