1999e8041e856d98f46095376bcc24fb33408795
Walter Silva <edweb.mza@gmail.com>
2024-11-06 07:33:43 -0300

    Probamos Combinacion de Selectores

--- clase11-JQuery-Selectores/index.js
+++ clase11-JQuery-Selectores/index.js
@@ -1,1 +1,12 @@
-//                                  jQuery
+//                                  jQuery y Selectores
+
+//                  Seleccionamos por Id
+$('#jquery1')
+
+//                  Seleccionamos por Class
+$('.ulFooter')
+
+//                  Seleccionamos por Tag
+$('li')
+
+
