f51e3e60754f392503a65dc717c15279310b7f8e
Walter Silva <edweb.mza@gmail.com>
2024-11-07 06:43:40 -0300

    jQuery Append() y correccion en el enlace de jQuery, va antes del script de js

--- clase11-JQuery-Selectores/index.js
+++ clase11-JQuery-Selectores/index.js
@@ -1,77 +1,84 @@
-//                                  jQuery y Selectores
+// //                                  jQuery y Selectores
 
-//                  Seleccionamos por Id
-$('#jquery1')
+// //                  Seleccionamos por Id
+// $('#jquery1')
 
-//                  Seleccionamos por Class
-$('.ulFooter')
+// //                  Seleccionamos por Class
+// $('.ulFooter')
 
-//                  Seleccionamos por Tag
-$('li')
-
-
-//                                  Combinacion de Selectores
-
-//                  Selecciona todos los p
-$('p')
-
-//                  Selecciona todos los a
-$('a')
-
-//                  Selecciona todos los <p> y los <a>
-$('p , a')
-
-//                  Seleccioan un ul con clase ulFooter, que tenga un hijo <li>
-$('ul.ulFooter li')
+// //                  Seleccionamos por Tag
+// $('li')
 
 
-//                                  Selectores Avanzados
+// //                                  Combinacion de Selectores
 
-$( "p:last" ); //Selecciona el último <p> de la página
+// //                  Selecciona todos los p
+// $('p')
 
-$( "li:first-child" ); //Selecciona todos los <li> que son primeros hijos
+// //                  Selecciona todos los a
+// $('a')
 
-$( "li:last-child" ); //Selecciona todos los <li> que son últimos hijos
+// //                  Selecciona todos los <p> y los <a>
+// $('p , a')
 
-$( "li:only-child" ); //Selecciona todos los <li> que sean hijos únicos
-
-$( "li:nth-child(3)" ); //Selecciona todos los <li> que sean el 3er elemento
-
-$( "tr:nth-child(odd)" ); //Selecciona todos los <tr> que sean impares
-
-$( "tr:nth-child(even)" ); //Selecciona todos los <tr> que sean pares
-
-$( "div:nth-child(3n)" ); //Selecciona cada tercer elemento <div>
+// //                  Seleccioan un ul con clase ulFooter, que tenga un hijo <li>
+// $('ul.ulFooter li')
 
 
-//                                  Selectores para Formularios
+// //                                  Selectores Avanzados
 
-$( ":text" );
+// $( "p:last" ); //Selecciona el último <p> de la página
 
-$( ":checkbox" );
+// $( "li:first-child" ); //Selecciona todos los <li> que son primeros hijos
 
-$( ":radio" );
+// $( "li:last-child" ); //Selecciona todos los <li> que son últimos hijos
 
-$( ":image" );
+// $( "li:only-child" ); //Selecciona todos los <li> que sean hijos únicos
 
-$( ":submit" );
+// $( "li:nth-child(3)" ); //Selecciona todos los <li> que sean el 3er elemento
 
-$( ":reset" );
+// $( "tr:nth-child(odd)" ); //Selecciona todos los <tr> que sean impares
 
-$( ":password" );
+// $( "tr:nth-child(even)" ); //Selecciona todos los <tr> que sean pares
 
-$( ":file" );
-
-$( ":input" ); //Selecciona los elementos input, textarea,select y button
-
-$( ":button" ); //Selecciona los elementos button e input con atributo "type"="button"
-
-$( ":enabled" ); //Selecciona los elementos del formulario activados
-
-$( ":disabled" ); //Selecciona los elementos del formulario desactivados
-
-$( ":checked" ); //Selecciona los radio buttons y checkboxes que están pulsados
-
-$( ":selected" ); //Elementos de una lista de opciones que este seleccionados
+// $( "div:nth-child(3n)" ); //Selecciona cada tercer elemento <div>
 
 
+// //                                  Selectores para Formularios
+
+// $( ":text" );
+
+// $( ":checkbox" );
+
+// $( ":radio" );
+
+// $( ":image" );
+
+// $( ":submit" );
+
+// $( ":reset" );
+
+// $( ":password" );
+
+// $( ":file" );
+
+// $( ":input" ); //Selecciona los elementos input, textarea,select y button
+
+// $( ":button" ); //Selecciona los elementos button e input con atributo "type"="button"
+
+// $( ":enabled" ); //Selecciona los elementos del formulario activados
+
+// $( ":disabled" ); //Selecciona los elementos del formulario desactivados
+
+// $( ":checked" ); //Selecciona los radio buttons y checkboxes que están pulsados
+
+// $( ":selected" ); //Elementos de una lista de opciones que este seleccionados
+
+
+//                                  jQuery Append()
+
+let producto = { id: 1, nombre: "Arroz", precio: 125 };
+//                      Es posible usar plantillas de texto en el parámetro.
+$("#app").append(`<div><h3> ID: ${producto.id}</h3>
+                <p> Producto: ${producto.nombre}</p>
+                <b> $ ${producto.precio}</b></div>`);
