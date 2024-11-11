ae2a621e38807dccfa7662f08e0b94d380194fcd
Walter Silva <edweb.mza@gmail.com>
2024-11-11 06:27:52 -0300

    Forma corta de escribir el método ready

---
+++ clase12-Selectores_y_Eventos/index.js
@@ -0,0 +1,19 @@
+//                  METODO .ready()
+
+$( document ).ready(function(){
+    console.log( "El DOM esta listo" );
+});
+
+
+//                  Forma corta de ready()
+$(function() {
+    console.log('El DOM esta listo');
+    });
+
+
+//                  Forma corta con arrow function
+$(() => {
+    console.log('El DOM esta listo');
+    });
+
+
