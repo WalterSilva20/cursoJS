caf60f32547f210e43c4c803b675265d5c2b52e8
Walter Silva <edweb.mza@gmail.com>
2024-11-12 10:57:55 -0300

    PRACTICA DE CLASE 12 CON NUEVOS PRODUCTOS

--- clase12-Selectores_y_Eventos/index.js
+++ clase12-Selectores_y_Eventos/index.js
@@ -35,22 +35,45 @@
 // //Asociamos el evento doble click al botón creado
 //     $('#btnjQuery').on('dblclick', () => {console.log("Respuesta al doble click");});
 
-const productos = [
-    { id: 1, nombre: "Arroz", precio: 125 },
-    { id: 2, nombre: "Fideo", precio: 70 },
-    { id: 3, nombre: "Pan" , precio: 50},
-    { id: 4, nombre: "Flan" , precio: 100}
+// const productos = [
+//     { id: 1, nombre: "Arroz", precio: 125 },
+//     { id: 2, nombre: "Fideo", precio: 70 },
+//     { id: 3, nombre: "Pan" , precio: 50},
+//     { id: 4, nombre: "Flan" , precio: 100}
+// ];
+
+// for (const producto of productos) {
+//     //Por cada producto además de los datos agregamos un botón
+//     $("#app").append(`<div>
+//                             <br>
+//                             <h4> Producto: ${producto.nombre}</h4>
+//                             <b> $ ${producto.precio}</b>
+//                             <br>
+//                             <button id="btn${producto.id}">Comprar</button>
+//     </div>`);
+
+//     $(`#btn${producto.id}`).on('click', ()=> {console.log(`Compraste ${producto.nombre}`)});
+//         }
+
+
+//                          PRACTICA CON NUEVOS PRODUCTOS
+
+const elementos = [
+    {id: 1, nombre: "plato", precio: 1500},
+    {id: 2, nombre: "vaso", precio: 1000},
+    {id: 3, nombre: "tenedor", precio: 1200},
+    {id: 4, nombre: "cuchillo", precio: 1300},
 ];
 
-for (const producto of productos) {
-    //Por cada producto además de los datos agregamos un botón
-    $("#app").append(`<div>
+for (const elemento of elementos) {
+    //Por cada elemento además de los datos agregamos un botón
+    $("#prod").append(`<div>
                             <br>
-                            <h4> Producto: ${producto.nombre}</h4>
-                            <b> $ ${producto.precio}</b>
+                            <h4> Producto: ${elemento.nombre}</h4>
+                            <b> $ ${elemento.precio}</b>
                             <br>
-                            <button id="btn${producto.id}">Comprar</button>
+                            <button id="btn${elemento.id}">Comprar</button>
     </div>`);
 
-    $(`#btn${producto.id}`).on('click', ()=> {console.log(`Compraste ${producto.nombre}`)});
+    $(`#btn${elemento.id}`).on('click', ()=> {console.log(`Compraste ${elemento.nombre}`)});
         }
