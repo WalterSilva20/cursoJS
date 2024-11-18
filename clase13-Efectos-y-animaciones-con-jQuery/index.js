93455936d50e1e1efa432960f4c8bffb5ec02231
Walter Silva <edweb.mza@gmail.com>
2024-11-18 08:58:05 -0300

    Cuando termina de ocultarse el elemento lo mostramos nuevamente

--- clase13-Efectos-y-animaciones-con-jQuery/index.js
+++ clase13-Efectos-y-animaciones-con-jQuery/index.js
@@ -16,8 +16,16 @@
 // //Mostramos con fadeIn() todos los <h3>
 // $("h3").fadeIn();
 
-//Agregemos <h3> con jQuery"
+// //Agregemos <h3> con jQuery"
+// $("body").prepend('<h3>¡Hola Coder1!</h3>');
+// $("body").prepend('<h3>¡Hola Coder2!</h3>');
+// //Ocultamos con fadeOut() todos los <h3>
+// $("h3").fadeOut();
+
+//Agregamos <h3> con jQuery
 $("body").prepend('<h3>¡Hola Coder1!</h3>');
-$("body").prepend('<h3>¡Hola Coder2!</h3>');
 //Ocultamos con fadeOut() todos los <h3>
-$("h3").fadeOut();
+$("h3").fadeOut("slow", function(){
+//Cuando termina de ocultarse el elemento lo mostramos nuevamente
+$("h3").fadeIn(1000);
+});
