69be79b18f0e420875743d6af3edda0ff1c7a589
Walter Silva <edweb.mza@gmail.com>
2024-11-18 08:56:14 -0300

    Ocultamos con fadeOut() todos los <h3>

--- clase13-Efectos-y-animaciones-con-jQuery/index.js
+++ clase13-Efectos-y-animaciones-con-jQuery/index.js
@@ -10,8 +10,14 @@
 // //Ocultamos con hide() todos los <h3>
 // $("h3").hide();
 
-//Agreguemos <h3> con jQuery ocultos con style="display: none;"
-$("body").prepend('<h3 style="display: none" >¡Hola Coder1!</h3>');
-$("body").prepend('<h3 style="display: none" >¡Hola Coder2!</h3>');
-//Mostramos con fadeIn() todos los <h3>
-$("h3").fadeIn();
+// //Agreguemos <h3> con jQuery ocultos con style="display: none;"
+// $("body").prepend('<h3 style="display: none" >¡Hola Coder1!</h3>');
+// $("body").prepend('<h3 style="display: none" >¡Hola Coder2!</h3>');
+// //Mostramos con fadeIn() todos los <h3>
+// $("h3").fadeIn();
+
+//Agregemos <h3> con jQuery"
+$("body").prepend('<h3>¡Hola Coder1!</h3>');
+$("body").prepend('<h3>¡Hola Coder2!</h3>');
+//Ocultamos con fadeOut() todos los <h3>
+$("h3").fadeOut();
