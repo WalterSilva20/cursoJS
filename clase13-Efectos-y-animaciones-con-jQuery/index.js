c1ea3b3037244766b4ef1ab416d4696038200962
Walter Silva <edweb.mza@gmail.com>
2024-11-18 08:55:15 -0300

    Ocultamos con hide() todos los <h3>

--- clase13-Efectos-y-animaciones-con-jQuery/index.js
+++ clase13-Efectos-y-animaciones-con-jQuery/index.js
@@ -1,5 +1,11 @@
-//Agregemos <h3> con jQuery ocultos con style="display: none;"
-$("body").prepend('<h3 style="display: none" >¡Hola Coder1!</h3>');
-$("body").prepend('<h3 style="display: none" >¡Hola Coder2!</h3>');
-//Mostramos con show() todos los <h3>
-$("h3").show();
+// //Agregemos <h3> con jQuery ocultos con style="display: none;"
+// $("body").prepend('<h3 style="display: none" >¡Hola Coder1!</h3>');
+// $("body").prepend('<h3 style="display: none" >¡Hola Coder2!</h3>');
+// //Mostramos con show() todos los <h3>
+// $("h3").show();
+
+//Agregamos <h3> con jQuery
+$("body").prepend('<h3>¡Hola Coder1!</h3>');
+$("body").prepend('<h3>¡Hola Coder2!</h3>');
+//Ocultamos con hide() todos los <h3>
+$("h3").hide();
