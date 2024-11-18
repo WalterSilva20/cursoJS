4621198cf08bddf026c5fe78790ee9b64e167def
Walter Silva <edweb.mza@gmail.com>
2024-11-18 07:55:17 -0300

    Evento click para btn2 con Arrow function y parámetro e

--- clase12-Selectores_y_Eventos/index.js
+++ clase12-Selectores_y_Eventos/index.js
@@ -35,45 +35,58 @@
 // //Asociamos el evento doble click al botón creado
 //     $('#btnjQuery').on('dblclick', () => {console.log("Respuesta al doble click");});
 
-const productos = [
-    { id: 1, nombre: "Arroz", precio: 125 },
-    { id: 2, nombre: "Fideo", precio: 70 },
-    { id: 3, nombre: "Pan" , precio: 50},
-    { id: 4, nombre: "Flan" , precio: 100}
-];
+// const productos = [
+//     { id: 1, nombre: "Arroz", precio: 125 },
+//     { id: 2, nombre: "Fideo", precio: 70 },
+//     { id: 3, nombre: "Pan" , precio: 50},
+//     { id: 4, nombre: "Flan" , precio: 100}
+// ];
 
-for (const producto of productos) {
-    //Por cada producto además de los datos agregamos un botón
-    $("#app").append(`<div>
-                            <br>
-                            <h4> Producto: ${producto.nombre}</h4>
-                            <b> $ ${producto.precio}</b>
-                            <br>
-                            <button id="btn${producto.id}">Comprar</button>
-    </div>`);
+// for (const producto of productos) {
+//     //Por cada producto además de los datos agregamos un botón
+//     $("#app").append(`<div>
+//                             <br>
+//                             <h4> Producto: ${producto.nombre}</h4>
+//                             <b> $ ${producto.precio}</b>
+//                             <br>
+//                             <button id="btn${producto.id}">Comprar</button>
+//     </div>`);
 
-    $(`#btn${producto.id}`).on('click', ()=> {console.log(`Compraste ${producto.nombre}`)});
-        }
+//     $(`#btn${producto.id}`).on('click', ()=> {console.log(`Compraste ${producto.nombre}`)});
+//         }
 
 
-//                          PRACTICA CON NUEVOS PRODUCTOS
+// //                  PRACTICA CON NUEVOS PRODUCTOS
 
-const elemen = [
-    {id: 1, nombre: "plato", precio: 1500},
-    {id: 2, nombre: "vaso", precio: 1000},
-    {id: 3, nombre: "tenedor", precio: 1200},
-    {id: 4, nombre: "cuchillo", precio: 1300},
-];
+// const elemen = [
+//     {id: 1, nombre: "plato", precio: 1500},
+//     {id: 2, nombre: "vaso", precio: 1000},
+//     {id: 3, nombre: "tenedor", precio: 1200},
+//     {id: 4, nombre: "cuchillo", precio: 1300},
+// ];
 
-for (const produ of elemen) {
+// for (const produ of elemen) {
 
-    $("#prod").append(`<div>
-                            <br>
-                            <h4> Producto: ${produ.nombre}</h4>
-                            <b> $ ${produ.precio}</b>
-                            <br>
-                            <button id="boton${produ.id}">Comprar</button>
-    </div>`);
+//     $("#prod").append(`<div>
+//                             <br>
+//                             <h4> Producto: ${produ.nombre}</h4>
+//                             <b> $ ${produ.precio}</b>
+//                             <br>
+//                             <button id="boton${produ.id}">Comprar</button>
+//     </div>`);
 
-    $(`#boton${produ.id}`).on('click', ()=> {console.log(`Compraste ${produ.nombre}`)});
-        }
+//     $(`#boton${produ.id}`).on('click', ()=> {console.log(`Compraste ${produ.nombre}`)});
+// }
+
+
+//                              Agregamos dos botones con jQuery
+$("body").prepend('<button id="btn1">BUTTON</button>');
+$("body").prepend('<button id="btn2">BUTTON</button>');
+//                              Asociamos el evento click para btn1
+$("#btn1").click(function () {
+console.log(this);
+});
+//                              Evento click para btn2 con Arrow function y parámetro e
+$("#btn2").click((e) => {
+console.log(e.target);
+});
