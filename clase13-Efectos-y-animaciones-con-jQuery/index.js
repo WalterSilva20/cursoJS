d0b7dac21a207c3e81e6172b97874d84f1a0d7e7
Walter Silva <edweb.mza@gmail.com>
2024-11-18 08:54:32 -0300

    Mostramos con show() todos los <h3>

---
+++ clase13-Efectos-y-animaciones-con-jQuery/index.js
@@ -0,0 +1,5 @@
+//Agregemos <h3> con jQuery ocultos con style="display: none;"
+$("body").prepend('<h3 style="display: none" >¡Hola Coder1!</h3>');
+$("body").prepend('<h3 style="display: none" >¡Hola Coder2!</h3>');
+//Mostramos con show() todos los <h3>
+$("h3").show();
