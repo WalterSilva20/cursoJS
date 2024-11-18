eecb2f84ff66839a3e67dbdfd23a930885641705
Walter Silva <edweb.mza@gmail.com>
2024-11-18 08:46:14 -0300

    EJEMPLO APLICADO: INPUT HIDDEN

--- clase12-Selectores_y_Eventos/index.js
+++ clase12-Selectores_y_Eventos/index.js
@@ -128,15 +128,45 @@
 
 
 
-    //Agregamos un botón y un input
-$("body").prepend('<button id="btn1">BUTTON</button>');
-$("body").prepend('<input id="ipt1" type="text">');
-//Asociamos el evento change al ipt1
-$("#ipt1").change((e) => {
-alert("El valor es " + e.target.value);
+//     //Agregamos un botón y un input
+// $("body").prepend('<button id="btn1">BUTTON</button>');
+// $("body").prepend('<input id="ipt1" type="text">');
+// //Asociamos el evento change al ipt1
+// $("#ipt1").change((e) => {
+// alert("El valor es " + e.target.value);
+// });
+// //Asociamos el evento click para btn1 y usamos trigger
+// $("#btn1").click(() => {
+// //Usamos trigger para disparar el evento change de ipt1
+// $("#ipt1").trigger("change");
+// });
+
+
+// Array de objetos para agregar información al DOM.
+const productos = [{ id: 1, nombre: "Arroz", precio: 125 },
+    { id: 2, nombre: "Fideo", precio: 70 },
+    { id: 3, nombre: "Pan" , precio : 50},
+    { id: 4, nombre: "Flan" , precio: 100}];
+
+// Asociamos el evento click en ready luego del DOM Generado
+$(document).ready(function () {
+$(".btnComprar ").click(function (e) {
+
+//Obtenemos hijos del padre <div> desde el target
+let hijos = $(e.target).parent().children();
+
+//Primer input, valor de ID oculto
+console.log(hijos[0].value);
 });
-//Asociamos el evento click para btn1 y usamos trigger
-$("#btn1").click(() => {
-//Usamos trigger para disparar el evento change de ipt1
-$("#ipt1").trigger("change");
 });
+// Recorremos el array con for..of
+for (const producto of productos) {
+
+//Por cada producto además de los datos agregamos un botón
+$("#app").append(`<div>
+    <input value=" ${producto.id}" type="hidden">
+    <h4> Producto: ${producto.nombre}</h4>
+    <b> $ ${producto.precio}</b>
+    <button class="btnComprar">Comprar</button>
+</div>` );
+}
