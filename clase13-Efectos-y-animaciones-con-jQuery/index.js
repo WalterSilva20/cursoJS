9853619103c5166c8149700ecbefa1cef0c0fcfc
Walter Silva <edweb.mza@gmail.com>
2024-11-18 09:01:03 -0300

    Usamos slideDown sobre div1 en respuesta al click del boton btn1

--- clase13-Efectos-y-animaciones-con-jQuery/index.js
+++ clase13-Efectos-y-animaciones-con-jQuery/index.js
@@ -1,6 +1,7 @@
 // //Agregemos <h3> con jQuery ocultos con style="display: none;"
 // $("body").prepend('<h3 style="display: none" >¡Hola Coder1!</h3>');
 // $("body").prepend('<h3 style="display: none" >¡Hola Coder2!</h3>');
+
 // //Mostramos con show() todos los <h3>
 // $("h3").show();
 
@@ -8,6 +9,7 @@
 // //Agregamos <h3> con jQuery
 // $("body").prepend('<h3>¡Hola Coder1!</h3>');
 // $("body").prepend('<h3>¡Hola Coder2!</h3>');
+
 // //Ocultamos con hide() todos los <h3>
 // $("h3").hide();
 
@@ -15,6 +17,7 @@
 // //Agreguemos <h3> con jQuery ocultos con style="display: none;"
 // $("body").prepend('<h3 style="display: none" >¡Hola Coder1!</h3>');
 // $("body").prepend('<h3 style="display: none" >¡Hola Coder2!</h3>');
+
 // //Mostramos con fadeIn() todos los <h3>
 // $("h3").fadeIn();
 
@@ -22,14 +25,17 @@
 // //Agregemos <h3> con jQuery"
 // $("body").prepend('<h3>¡Hola Coder1!</h3>');
 // $("body").prepend('<h3>¡Hola Coder2!</h3>');
+
 // //Ocultamos con fadeOut() todos los <h3>
 // $("h3").fadeOut();
 
 
 // //Agregamos <h3> con jQuery
 // $("body").prepend('<h3>¡Hola Coder1!</h3>');
+
 // //Ocultamos con fadeOut() todos los <h3>
 // $("h3").fadeOut("slow", function(){
+
 // //Cuando termina de ocultarse el elemento lo mostramos nuevamente
 // $("h3").fadeIn(1000);
 // });
@@ -41,6 +47,7 @@ $("body").prepend(`<div id="div1" style="height: 120px">
 <h3>¡Hola Coder!</h3>
 <h4>Sorpresa 2</h4>
 </div>`);
+
 //Usamos slideDown sobre div1 en respuesta al click del boton btn1
 $("#btn1").click(() => {
 $("#div1").slideDown("fast");
